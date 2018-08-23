a370b05057a2a2d19fbf6be91dc1dc61022337ed
Daniel Sisk <dsisk559@gmail.com>
2018-08-23 16:26:02 -0700

    Completed header/nav

---
+++ source/js/app.js
@@ -0,0 +1,14 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menuBtn.classList.toggle("close");
+  menu.classList.toggle("show");
+  menuNav.classList.toggle("show");
+  navItems.forEach(item => item.classList.toggle("show"));
+}
